0b63fa64a82ba7c93c7c696acda8bbc67db97fe1
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2018-05-24 08:47:31 +0200

    adding special hands tests

--- src/Card.hs
+++ src/Card.hs
@@ -4,14 +4,15 @@ import Suit
 import Rank
 
 import Data.List
+import qualified Data.Text as T
 
 data Card = Card Rank Suit deriving (Eq)
 
 instance Show Card where
     show (Card r s) = show r ++ show s
 
-parseCard :: [Char] -> Card
-parseCard s = Card (parseRank (s!!0)) (parseSuit (s!!1))  
+parseCard :: T.Text -> Card
+parseCard s = Card (parseRank (T.index s 0)) (parseSuit (T.index s 1))  
 
 suit :: Card -> Suit
 suit (Card _ s) = s 
@@ -24,8 +25,8 @@ allCards = [Card r s | s <- allSuits, r <- allRanks]
 
 type Hand = [Card]
 
---parseHand :: [Char] -> Hand
---parseHand s = map parseCard $ splitOn " " s
+parseHand :: String -> Hand
+parseHand s = map parseCard $ T.splitOn (T.pack " ") (T.pack s)
 
 groupedByRank :: Hand -> [[Rank]]
 groupedByRank hand = groupBy (==) $ map rank hand
@@ -39,3 +40,22 @@ isXOfAKind x hand
 isFourOfAKind = isXOfAKind 4
 isThreeOfAKind = isXOfAKind 3
 isTwoOfAKind = isXOfAKind 2
+
+isFullHouse :: Hand -> Maybe (Rank, Rank)
+isFullHouse hand = do
+    three <- isThreeOfAKind hand
+    two <- isTwoOfAKind hand
+    return (three, two)
+
+allPossibleStraights = 
+    [ straight | 
+        startRank <- enumFromTo Two Nine,
+        let endRank = toEnum ((fromEnum startRank) + 4),
+        let straight = enumFromTo startRank endRank
+    ]
+
+isStraight :: Hand -> Maybe Rank
+isStraight hand = let 
+    sortedRank = sort (map rank hand)
+    isStraight = any (sortedRank ==) allPossibleStraights
+    in if isStraight then Just (head sortedRank) else Nothing
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,28 @@
 import HandGenerator
 
+import Test.QuickCheck
 import Test.Hspec (Spec, hspec, describe, it, shouldBe)
 
-import Test.QuickCheck
-
+import Suit
+import Rank
+import Card
+import HandGenerator
 
 main :: IO ()
 main = hspec $ do
-    describe "Fake test" $ do
-        it "should simulate test" $ do
-            False `shouldBe` True
+    describe "Poker Special Hand" $ do
+        it "should have four of a kind" $ do
+            let hand = parseHand "A♣ A♥ A♦ A♠ T♥"
+            isFourOfAKind hand `shouldBe` Just Ace
+        it "should have three of a kind" $ do
+            let hand = parseHand "3♣ 3♥ 3♦ 4♠ T♥"
+            isThreeOfAKind hand `shouldBe` Just Three
+        it "should have two of a kind" $ do
+            let hand = parseHand "J♣ J♥ Q♦ K♠ T♥"
+            isTwoOfAKind hand `shouldBe` Just Jack
+        it "should have full house" $ do
+            let hand = parseHand "J♣ J♥ Q♦ Q♠ Q♥"
+            isFullHouse hand `shouldBe` Just (Queen, Jack)
+        it "should have straight" $ do
+            let hand = parseHand "4♣ 5♥ 6♦ 7♠ 8♥"
+            isStraight hand `shouldBe` Just Four
